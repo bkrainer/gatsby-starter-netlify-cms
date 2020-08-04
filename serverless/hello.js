c22ca1a6ac41cebc05c2740076d336ba32d737b4
Brian Krainer <bkrainer@hubspot.com>
2020-08-04 13:53:02 -0400

    add hs dep

--- serverless/hello.js
+++ serverless/hello.js
@@ -1,6 +1,8 @@
 var _ = require('lodash');
+var hs = require('hubspot');
 
 exports.handler = function(event, context, callback) {
+    console.log(hs);
 		var thing = event.queryStringParameters.name;
     callback(null, {
     statusCode: 200,
