00ac4aac496514d5dc1723e2fc674b6f9dd56629
Brian Krainer <bkrainer@hubspot.com>
2020-08-04 14:30:24 -0400

    nested lambda path

--- serverless/thing/hello.js
+++ serverless/thing/hello.js
@@ -2,6 +2,6 @@ exports.handler = function(event, context, callback) {
 		var thing = event.queryStringParameters.name;
     callback(null, {
     statusCode: 200,
-    body: "Path: " + event.path
+    body: "nested lambda"
     });
 }
