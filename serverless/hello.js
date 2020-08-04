b6ce02f9b8491867d5e73b00c77bfe5cf19004da
Brian Krainer <bkrainer@hubspot.com>
2020-08-04 14:00:53 -0400

    path

--- serverless/hello.js
+++ serverless/hello.js
@@ -6,6 +6,6 @@ exports.handler = function(event, context, callback) {
 		var thing = event.queryStringParameters.name;
     callback(null, {
     statusCode: 200,
-    body: "Hello, " +  thing + " " +  _.now()
+    body: "Path: " + event.path
     });
 }
